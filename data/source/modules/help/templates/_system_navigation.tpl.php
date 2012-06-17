38f841aab24a219971ceb906c2e0f6521b6bbe87
Nicolas Frinker <nicofrix@nif7.de>
2012-06-17 07:20:59 +0200

    help: style fixes

--- data/source/modules/help/templates/_system_navigation.tpl.php
+++ data/source/modules/help/templates/_system_navigation.tpl.php
@@ -1,13 +1,13 @@
 <!-- | -->
 <div id="$$$div___navigation">
-	<ul>
-		<li id="$$$_navigation__showMain"><a href="<?php $this->setUrl('$$$showMain'); ?>"><?php $this->set('{_SYSTEM_NAVIGATION__SHOWMAIN}'); ?></a></li>
-	</ul>
+    <ul>
+	<li id="$$$_navigation__showMain"><a href="<?php $this->setUrl('$$$showMain'); ?>"><?php $this->set('{_SYSTEM_NAVIGATION__SHOWMAIN}'); ?></a></li>
+    </ul>
 </div>
 
 <script type="text/javascript">
 
-	// add events
-	document.getElementById('$$$_navigation__showMain').onclick = function(){location.href='<?php $this->setUrl('$$$showMain', false, false); ?>';};
+    // add events
+    document.getElementById('$$$_navigation__showMain').onclick = function(){location.href='<?php $this->setUrl('$$$showMain', false, false); ?>';};
 
 </script>
