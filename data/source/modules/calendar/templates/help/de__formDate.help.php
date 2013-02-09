b83e0a6ea035725a24088c4a05781a03b8bee2c3
Nicolas Frinker <nicofrix@nif7.de>
2013-02-09 19:44:42 +0100

    calendar: Add help for formDate

---
+++ data/source/modules/calendar/templates/help/de__formDate.help.php
@@ -0,0 +1,28 @@
+<!-- | HELP fromDate -->
+<h3>Das Formular</h3>
+<dl>
+    <dt>Titel</dt>
+    <dd>
+	Name für den Termin
+    </dd>
+    <dt>Startzeitpunkt</dt>
+    <dd>
+	Zeitpunkt, an dem der Termin startet.
+    </dd>
+    <dt>Endzeitpunkt</dt>
+    <dd>
+	Zeitpunkt, an dem der Termin endet.
+    </dd>
+    <dt>Wiederhole alle...</dt>
+    <dd>
+	Art der Wiederholung (z.B. wiederhole alle "2 Tage", "3 Montate", "4 Jahre")
+    </dd>
+    <dt>...x mal</dt>
+    <dd>
+	Wiederhole den Termin x mal.
+    </dd>
+    <dt>...bis</dt>
+    <dd>
+	Wiederhole Termin bis bestimmtes Datum erreicht ist.
+    </dd>
+</dl>
---
+++ data/source/modules/calendar/templates/help/en__formDate.help.php
@@ -0,0 +1,28 @@
+<!-- | HELP fromDate -->
+<h3>The form</h3>
+<dl>
+    <dt>Title</dt>
+    <dd>
+	Title for date
+    </dd>
+    <dt>Start of date</dt>
+    <dd>
+	When does the date start?
+    </dd>
+    <dt>End of date</dt>
+    <dd>
+	When does the date end?
+    </dd>
+    <dt>Repeat every...</dt>
+    <dd>
+	Type of repeatition (e.g. repeat every "2 days", "3 months", "4 years")
+    </dd>
+    <dt>...x times</dt>
+    <dd>
+	Repeat date x times.
+    </dd>
+    <dt>...until</dt>
+    <dd>
+	Repeat date until reached certain time.
+    </dd>
+</dl>
