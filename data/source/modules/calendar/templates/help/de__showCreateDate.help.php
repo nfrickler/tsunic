4c06debfde1fb96246cd0c175e3851d9051088c0
Nicolas Frinker <nicofrix@nif7.de>
2013-02-10 20:01:13 +0100

    calendar: add help for showCreateDate

---
+++ data/source/modules/calendar/templates/help/de__showCreateDate.help.php
@@ -0,0 +1,6 @@
+<!-- | HELP create new date -->
+<h2>Neue Termin anlegen</h2>
+<p>
+    Mithilfe des folgenden Formulars kann ein neuer Termin angelegt werden.
+</p>
+<?php include '$$$de__formDate.help.php'; ?>
---
+++ data/source/modules/calendar/templates/help/en__showCreateDate.help.php
@@ -0,0 +1,6 @@
+<!-- | HELP create new date -->
+<h2>Create new date</h2>
+<p>
+    Via the following form a new date can be created.
+</p>
+<?php include '$$$en__formDate.help.php'; ?>
