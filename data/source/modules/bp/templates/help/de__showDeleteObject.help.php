d6d35cebcaed95ef8a8cbbe36e5ef15444fe40b4
Nicolas Frinker <nicofrix@nif7.de>
2013-02-17 20:00:15 +0100

    bp: add help for showDeleteObject

---
+++ data/source/modules/bp/templates/help/de__showDeleteObject.help.php
@@ -0,0 +1,5 @@
+<!-- | HELP showDeleteObject -->
+<h2>Objekt löschen</h2>
+<p>
+    Bestätige hier, dass du das Objekt wirklich löschen willst.
+</p>
---
+++ data/source/modules/bp/templates/help/en__showDeleteObject.help.php
@@ -0,0 +1,5 @@
+<!-- | HELP showDeleteTag -->
+<h2>Delete Object</h2>
+<p>
+    Confirm to really delete this Object.
+</p>
